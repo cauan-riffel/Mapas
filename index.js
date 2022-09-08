01118518e361407557f734cd41c2f4d90185c0b8
Nauac <83404051+nauacbr@users.noreply.github.com>
2022-09-08 00:44:02 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+let zoons = [],
+    zoomAtual = 3,
+    mapa = document.getElementById("imgPrincipal")
+    
+document.body.addEventListener('drop', ()=>{mouse=0})
+
+for(let i=0; ++i<=12;){
+    zoons.push(12*i);
+}
+
+function alterarZoom(tipo){
+    if(tipo){
+        if(zoomAtual<12) zoomAtual++;
+        mapa.style.zoom = zoons[zoomAtual]+"%";
+    }else{
+        if(zoomAtual>0) zoomAtual--;
+        mapa.style.zoom = zoons[zoomAtual]+"%";
+    }
+}
+
+let liberado = 0,
+    mouse
+
+const CONTROLE = document.getElementById("mapaContent")
+
+async function mover(evento){
+    mapa.style.cursor = "pointer";
+    if(!mouse){
+        mouse=[evento.offsetX, evento.offsetY];
+    }
+    else{
+        let novoMouse=[evento.offsetX, evento.offsetY],
+            posicao=[CONTROLE.scrollLeft, CONTROLE.scrollTop];
+        console.log(novoMouse);
+        if((novoMouse[0]-mouse[0]>60 || novoMouse[0]-mouse[0]<-60) && (novoMouse[1]-mouse[1]>60 || novoMouse[1]-mouse[1]<-60)){
+            mouse=0;
+            mapa.style.cursor = "auto";
+            return;
+        }
+        CONTROLE.scroll(posicao[0]-(novoMouse[0]-mouse[0]), posicao[1]-(novoMouse[1]-mouse[1]));
+    }
+}
+
+function verDados(local){
+    for(let i in local){
+        if(typeof local[i] != "object" && typeof local[i] != "function") console.log(i+" -> "+local[i])
+    }
+}
+
